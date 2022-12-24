28bb9ee65f94cc37a7775986e7da166b1d70f319
Islacrusez <m4rek@hotmail.com>
2022-12-24 13:36:43 +0000

    Layout draft

--- app/main.rb
+++ app/main.rb
@@ -19,6 +19,20 @@ def main_menu(args)
 end
 
 def game_run(args)
+	# render calls
+	game_render(args)
+	
+	return if args.state.game_paused
+	# game logic
 
+end
 
+def game_render(args)
+	# layout
+	x_split = 8
+	height = 12
+	width = 24
+	args.outputs.borders << args.layout.rect(row: 0, col: x_split, w: width - x_split, h: height) # Right 
+	args.outputs.borders << args.layout.rect(row: 0, col: 0, w: x_split, h: height) # Left 
+	
 end
